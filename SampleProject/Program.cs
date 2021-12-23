7bd4dc11ebed0b2fc3ffdfaff52ba0c660095d87
Jannis meierhoefer <jannism1@web.de>
2021-12-23 19:04:20 +0100

    Add project files.

---
+++ SampleProject/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SampleProject;
+using System.Text.Json;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var client = new HttpClient();
+var authresult = await  client.PostAsync("https://eu-central-1.aws.realm.mongodb.com/api/client/v2.0/app/kiloforkilo-jcdvl/auth/providers/anon-user/login", null);
+var authcontent = await authresult.Content.ReadAsStringAsync();
+var result = JsonSerializer.Deserialize<AuthenticatedUser>(authcontent);
+builder.Services
+    .AddSampleClient()
+    .ConfigureHttpClient(client =>
+    {
+        client.BaseAddress = new Uri("https://eu-central-1.aws.realm.mongodb.com/api/client/v2.0/app/kiloforkilo-jcdvl/graphql");
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",result.access_token);
+    });
+
+await builder.Build().RunAsync();
